9be84afff11c4e68cc5187238eae871048b63940
hgonzlezcaldern <hernan.gonzalez.calderon@gmail.com>
2025-02-05 21:22:18 +0100

    Interactuable crate and crate UI. It shows interacting player inventory and crate inventory.

---
+++ Plugins/GameFeatures/ShooterCore/Source/ShooterCoreRuntime/Public/LyraWorldItemContainer.h
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+// Unreal Engine
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+
+// Lyra Project
+#include "Interaction/IInteractableTarget.h"
+#include "LyraWorldItemContainer.generated.h"
+
+// Lyra Project
+class ULyraInventoryItemInstance;
+class ULyraInventoryItemDefinition;
+
+UCLASS()
+class SHOOTERCORERUNTIME_API ALyraWorldItemContainer : public AActor, public IInteractableTarget
+{
+	GENERATED_BODY()
+
+public:
+	ALyraWorldItemContainer();
+
+	virtual void GatherInteractionOptions(const FInteractionQuery& InteractQuery, FInteractionOptionBuilder& InteractionBuilder) override;
+
+protected:
+	UPROPERTY(EditAnywhere)
+	FInteractionOption Option;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TArray<TSubclassOf<ULyraInventoryItemDefinition>> ItemDefinitions;
+};
